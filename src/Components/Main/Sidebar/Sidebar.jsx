e27767c3b7c6922bebef7373923718afc7947e53
Alekseymedvedev <alekseymedvedev152@gmail.com>
2021-09-07 21:00:38 +0300

    NavLink

--- src/Components/Main/Sidebar/Sidebar.jsx
+++ src/Components/Main/Sidebar/Sidebar.jsx
@@ -1,19 +1,23 @@
+import React from 'react';
+import classes from "./Sidebar.module.css";
+import {NavLink} from 'react-router-dom';
+
 const Sidebar = () => {
     return (
-        <aside className="sidebar">
+        <aside className={classes.sidebar}>
             <nav>
                 <ul>
-                    <li className="list">
-                        <a href="/profile">Профиль</a>
+                    <li className={classes.list}>
+                        <NavLink to="/profile" activeClassName={classes.active}>Профиль</NavLink>
                     </li>
-                    <li className="list">
-                        <a href="/messages">Сообщения</a>
+                    <li className={classes.list}>
+                        <NavLink to="/messages" activeClassName={classes.active}>Сообщения</NavLink>
                     </li>
-                    <li className="list">
-                        <a href="№">Новости</a>
+                    <li className={classes.list}>
+                        <NavLink to="/ffff" activeClassName={classes.active}>Новости</NavLink>
                     </li>
-                    <li className="list">
-                        <a href="№">настройка</a>
+                    <li className={classes.list}>
+                        <NavLink to="/messages" activeClassName={classes.active}>настройка</NavLink>
                     </li>
                 </ul>
             </nav>
